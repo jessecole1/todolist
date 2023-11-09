5b00543d1940eee5fa48066136d31fa6aee0fa41
jesse cole <cole.j.jesse@gmail.com>
2023-11-08 23:45:18 -0800

    changed app.listen back to 8000

--- server/index.js
+++ server/index.js
@@ -15,4 +15,4 @@ app.use(express.json(), express.urlencoded({ extended : true }));
 const AllMyTodoRoutes = require("./routes/todo.routes");
 AllMyTodoRoutes(app);
 
-app.listen(db, () => console.log("The server is fired up and running on port 8000"));
+app.listen(8000, () => console.log("The server is fired up and running on port 8000"));
