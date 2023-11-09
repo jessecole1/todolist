48cd6932bedd08e7ddd16a67e863da843a9fc684
jesse cole <cole.j.jesse@gmail.com>
2023-11-09 00:29:21 -0800

    removed brackets

--- server/config/mongoose.config.js
+++ server/config/mongoose.config.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://colejjesse:<renderpassword123!>@cluster0.qxixjla.mongodb.net/', {
+mongoose.connect('mongodb+srv://colejjesse:renderpassword123!@cluster0.qxixjla.mongodb.net/', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 }) 
