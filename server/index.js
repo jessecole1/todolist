ac9764a16804dc81f85619662f282c314f70e803
jesse cole <cole.j.jesse@gmail.com>
2023-11-08 23:41:07 -0800

    testing, changed app.listen to mongo atlas db url

--- server/index.js
+++ server/index.js
@@ -2,6 +2,10 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 
+let db = 'mongodb+srv://colejjesse:<password>@cluster0.qxixjla.mongodb.net/';
+
+
+
 app.use(cors({ origin:"http://localhost:3000"}));
 
 require('./config/mongoose.config');
@@ -11,4 +15,4 @@ app.use(express.json(), express.urlencoded({ extended : true }));
 const AllMyTodoRoutes = require("./routes/todo.routes");
 AllMyTodoRoutes(app);
 
-app.listen(8000, () => console.log("The server is fired up and running on port 8000"));
+app.listen(db, () => console.log("The server is fired up and running on port 8000"));
